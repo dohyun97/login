39fdcec829efb060a5290ebb2d6c7a8a6ae46e84
dohyun97 <dohyun647@gmail.com>
2023-01-20 15:35:38 -0500

    With Axios

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const user = [
+    {
+        name:"user1",
+        id:"abcdef",
+        password:"123456789"
+    },
+    {
+        name:"user2",
+        id:"abcdefg",
+        password:"111111111"
+    }
+]
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.get('/api/user', (req, res) => {
+  res.send(user)
+})
+
+app.post('/api/user',(req,res) =>{
+    
+    user.push(req.body)
+    res.send(user)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
